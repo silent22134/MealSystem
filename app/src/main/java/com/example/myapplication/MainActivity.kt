43d3f1948b779f21456e15e8cccde538a99d45fd
silent22134 <firenba46@gmail.com>
2022-12-02 14:32:09 +0800

    Meal System

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnMeal = findViewById<Button>(R.id.btn_meal)
+        var btnDrink = findViewById<Button>(R.id.btn_drink)
+        var tableNo = findViewById<EditText>(R.id.ed_tableNo)
+        var tvMeal = findViewById<TextView>(R.id.tv_meal)
+        var txtTableNo = tableNo.text
+        var tvDrink = findViewById<TextView>(R.id.tv_drink)
+        var btnReorder = findViewById<Button>(R.id.btn_reOrder)
+
+        btnMeal.setOnClickListener{
+            if(tableNo.length()<1)
+                tableNo.hint = "請輸入桌號!"
+            else{
+            }
+        }
     }
 }
