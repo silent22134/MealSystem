d2e0f5aee40e3d2bd9e8849643b019fbb7aa49d5
silent22134 <firenba46@gmail.com>
2022-12-02 13:37:07 +0800

    Meal System

---
+++ app/src/main/java/com/example/myapplication/Main2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
