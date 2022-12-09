8acc0efb57d539e516d1277683912464e8cc2ad7
silent22134 <firenba46@gmail.com>
2022-12-09 14:47:04 +0800

    Activity2

--- app/src/main/java/com/example/myapplication/Main3Activity.kt
+++ app/src/main/java/com/example/myapplication/Main3Activity.kt
@@ -2,10 +2,21 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class Main3Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        var tvTable3 = findViewById<TextView>(R.id.tv_table3)
+        var btnDrinkConfirm = findViewById<Button>(R.id.btn_drinkConfirm)
+
+        tvTable3.text = intent.getStringExtra("dataTransfer")
+
+        btnDrinkConfirm.setOnClickListener {
+            var selectedOption : Int =
+        }
     }
 }
